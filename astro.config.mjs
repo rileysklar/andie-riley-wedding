9425984677a8dec372cf8593334a4ac282f523b8
Riley Sklar <riley.sklar@gmail.com>
2024-05-17 19:44:12 -0500

    url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://highdesertsklars.netlify.app/",
   integrations: [mdx(), sitemap(), tailwind(), react()],
 });
