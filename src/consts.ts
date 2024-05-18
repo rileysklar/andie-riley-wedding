be5d5466ef1798633d60822e3ad789520e070917
Riley Sklar <riley.sklar@gmail.com>
2024-05-17 19:18:34 -0500

    meta

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Riley & Andie";
-export const SITE_DESCRIPTION = "We are getting married!";
+export const SITE_TITLE = "High Desert Sklars";
+export const SITE_DESCRIPTION =
+  "Riley & Andie are getting married in Taos, New Mexico on April 12, 2025!";
