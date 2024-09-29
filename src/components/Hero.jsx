a1b22f9f2fbdaacad606a0c5c5049272768710de
Riley Sklar <riley.sklar@gmail.com>
2024-09-29 12:22:50 -0500

    updated hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,23 +2,33 @@ import Countdown from "./Countdown";
 
 export default function Hero() {
   return (
-    <div className="hero-gradient gap-4 text-[#fff6e4] flex flex-col items-end align-center justify-end bg-cover bg-top h-[80svh] flex items-center p-8">
-      <div className="flex flex-col md:flex-row w-full">
-        <div className="flex-1 text-left">
-          <h1 className="text-[#2d2d2d] text-6xl mb-4 drop-shadow-md">
-            We're getting married!
-          </h1>
-          <p className="text-[#0b0b0b] text-2xl mb-0 drop-shadow-md">
-            Meet us at Fiesta in San Antonio
-          </p>
-        </div>
-        <div className="flex-1"></div> {/* Empty column for spacing */}
-      </div>
+    <div class="hero-gradient gap-4 text-[#1a1a1a] flex flex-col align-center justify-center bg-cover bg-top h-[80svh] flex items-center p-8">
+      <h1
+        class="text-shadow-lg text-center text-6xl mb-4 drop-shadow-md"
+        style={{
+          textShadow:
+            "2px 2px 0 #fff, -2px -2px 0 #fff, 2px -2px 0 #fff, -2px 2px 0 #fff",
+        }}
+      >
+        We're getting married!
+      </h1>
+
+      <p
+        className="text-2xl text-shadow-lg text-center mb-0 drop-shadow-md p-4 rounded-lg"
+        style={{
+          background: "rgba(255, 255, 255, 0.15)",
+          boxShadow: "0 8px 32px 0 rgba(31, 38, 135, 0.17)",
+          backdropFilter: "blur(4px)",
+          WebkitBackdropFilter: "blur(4px)",
+        }}
+      >
+        Meet us at Fiesta in San Antonio
+      </p>
       <a
         className="flex flex-col items-center align-center justify-center gap-4"
         href="#rsvp"
       >
-        {/* <button className="text-[#fff6e4] bg-black pt-2 pb-1 px-4 mb-0 rounded-full text-2xl text-center drop-shadow-md">
+        {/* <button class="text-[#fff6e4] bg-black pt-2 pb-1 px-4 mb-0 rounded-full text-2xl text-center drop-shadow-md">
           April 12, 2025
         </button> */}
         {/* <Countdown /> */}
